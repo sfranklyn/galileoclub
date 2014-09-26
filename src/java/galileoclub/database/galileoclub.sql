6cd93ba7e22ba5536ddd886ade04ceaa9231976a
Samuel Franklyn <sfranklyn@gmail.com>
2014-09-26 09:10:25 +0700

    Add Users Pccs Table User Interface

--- src/java/galileoclub/database/galileoclub.sql
+++ src/java/galileoclub/database/galileoclub.sql
@@ -107,7 +107,7 @@ create table users_pccs (
     pccs_id int not null,
     user_pcc_son varchar(10) not null,
     user_pcc_version int not null default 0,
-    primary key (user_id,pccs_id),
+    primary key (user_id,pccs_id,user_pcc_son),
     constraint fk_users_pccs1 foreign key (user_id) references users (user_id),
     constraint fk_users_pccs2 foreign key (pccs_id) references pccs (pccs_id),
     index idx_users_pccs1 (user_id),
